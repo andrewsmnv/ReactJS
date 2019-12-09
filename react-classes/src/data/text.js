ce31b4d053675906eff94140f7cdf3fee5976858
Andrew Semenov <exe312@gmail.com>
2019-12-10 00:11:25 +0200

    Changed text.js a little bit

--- react-classes/src/data/text.js
+++ react-classes/src/data/text.js
@@ -1,11 +1,11 @@
 const text = [
   {
     string: "This string was created with class extends React.Component",
-    anotherString: "This one also"
+    anotherString: "This one also!"
   },
   {
     string: "I've had to use .map method",
-    anotherString: "So here we are"
+    anotherString: "So here we are "
   }
 ];
 
